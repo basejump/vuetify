9d43bc7fdde0fc2082cd0e51388115d8c3982e98
Joshua B <joshdev@9ci.com>
2017-06-04 03:52:03 -0500

    theme poc phase 1 for buttons, toolbar, nav-drawer

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,9 +1,10 @@
 import Themeable from '../../mixins/themeable'
+import Contextualable from '../../mixins/contextualable'
 
 export default {
   functional: true,
 
-  mixins: [Themeable],
+  mixins: [Themeable, Contextualable],
 
   props: {
     fixed: Boolean
@@ -12,8 +13,13 @@ export default {
   render (h, { data, children, props }) {
     data.staticClass = data.staticClass ? `toolbar ${data.staticClass}` : 'toolbar'
     if (props.fixed) data.staticClass += ' toolbar--fixed'
-    data.staticClass += props.light ? ' toolbar--light' : ' toolbar--dark'
-
+    //data.staticClass += props.light ? ' toolbar--light' : ' toolbar--dark'
+    console.log("props", props)
+    let tones = ['primary', 'secondary', 'success', 'info', 'warning', 'error']
+    tones.forEach(tone => {
+      console.log("prop", props[tone])
+      if(props[tone]) data.staticClass += (' '+ tone)
+    })
     return h('nav', data, children)
   }
 }
